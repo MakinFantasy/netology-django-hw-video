baf05093367916ef27a58d071920f4234d03b483
MakinFantasy <beinp0z1t1v3@gmail.com>
2022-04-17 19:42:19 +0300

    Added serializers and path for demo application. Also created several objects for Sensor class. Rename application, create objects for Measurements class

--- 3.1-drf-intro/smart_home/measurement/serializers.py
+++ 3.1-drf-intro/smart_home/measurement/serializers.py
@@ -16,3 +16,10 @@ class SensorDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Sensor
         fields = ['id', 'name', 'description', 'measurements']
+
+
+class SensorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Sensor
+        fields = ['id', 'name', 'description']
+
--- 3.1-drf-intro/smart_home/measurement/views.py
+++ 3.1-drf-intro/smart_home/measurement/views.py
@@ -1,2 +1,17 @@
 # TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
 # TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+from rest_framework.response import Response
+# from rest_framework.generics import
+from rest_framework.decorators import api_view
+from .models import Sensor
+from .serializers import SensorSerializer
+
+
+@api_view(['GET', 'POST'])
+def demo(request):
+    if request.method == 'GET':
+        sensors = Sensor.objects.all()
+        ser = SensorSerializer(sensors, many=True)
+        return Response(ser.data)
+    if request.method == 'POST':
+        return Response({'status': 'OK'})
